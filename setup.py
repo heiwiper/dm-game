f7ea1e07c73d7b8077041945d8f2de51ec07f13a
Hei Wiper <heiwiper@gmail.com>
2020-10-02 13:26:47 +0100

    Modified build file

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from cx_Freeze import setup,Executable
+from cx_Freeze import Executable, setup
 
 base = None
 
 setup(name="Data Mining Game",
 		 options={"build_exe":{"packages":["pygame","random","efficient_apriori","re","collections"],
-		 					"include_files":["bgm.mp3","objects.txt","assets","difficulty"]}},
-         version="1.0",
-         description="as above",
-         executables=[Executable("game.py", base=base)])
+		 					"include_files":["objects.txt","assets","difficulty"]}},
+         version="0.1",
+         description="This game implements Apriori algorithm from data mining class.",
+         author="Mohammed Benzouache & Abdallah Maouche",
+         executables=[Executable("game.py", base=base, icon="icon.ico")])
